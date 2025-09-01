4368a8d810bc09a181d7ef80b12eae6cebf0d9ac
serhiybondarenkogithub <sergiogetstudy@gmail.com>
2025-09-01 14:08:00 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Swiper from "swiper";
+import { Mousewheel } from "swiper/modules";
+
+new Swiper("[data-swiper]", {
+  modules: [Mousewheel],
+  direction: "vertical",
+  slidesPerView: 1,
+  mousewheel: true,
+  cssMode: true,
+  speed: 700,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
